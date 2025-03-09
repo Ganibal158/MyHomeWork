7e697ec0d1cff20f3bf0f315606c5ea87b19eb17
Kirill <kuznetsokirill@yandex.ru>
2025-03-09 21:01:25 +0300

    HW3 is changed3

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,54 +1,30 @@
 package hw03frequencyanalysis
 
 import (
-	"fmt"
 	"sort"
 	"strings"
 )
 
 func Top10(str1 string) []string {
-	var top1 []string
-	s := strings.Fields(str1) // слайс из исходной строки
-	var s2 []int              // слайс для подсчёта частоты слова
-	var s3 []string           // слайс уникальных значений
-	for i, val1 := range s {
-		index := 0
-		check := true
-		if i == 0 {
-			s3 = append(s3, val1)
-			for _, val2 := range s {
-				if val2 == val1 {
-					index++
-				}
-			}
-			s2 = append(s2, index)
-		}
-		for _, valU := range s3 { // проверка на уникальность
-			if val1 == valU {
-				check = false
-			}
-		}
-		if check { // Если уникально
-			s3 = append(s3, val1)
-			for _, val2 := range s {
-				if val2 == val1 {
-					index++
-				}
-			}
-			s2 = append(s2, index)
-		}
+	if len(str1) == 0 {
+		return nil
 	}
-	WordAndIndex := make(map[string]int)
-	for id := range s2 {
-		WordAndIndex[s3[id]] = s2[id]
+	var top1 []string
+	s := strings.Fields(str1)            // слайс из исходной строки
+	WordAndIndex := make(map[string]int) // мапа с уникальными словами и их количеством
+	for _, word := range s {
+		WordAndIndex[word]++
+	}
+	var s3 []string // слайс с уникальными словами
+	for key := range WordAndIndex {
+		s3 = append(s3, key)
 	}
 	sort.Slice(s3, func(i, j int) bool {
-		if WordAndIndex[s3[i]] != WordAndIndex[s3[j]] {
+		if WordAndIndex[s3[i]] != WordAndIndex[s3[j]] { //сортировка по количеству совпадений
 			return WordAndIndex[s3[i]] > WordAndIndex[s3[j]]
 		}
-		return s3[i] < s3[j]
+		return s3[i] < s3[j] // сортировка лексикографическая
 	})
-	fmt.Println(s3)
 	var k int
 	if len(s3) < 10 {
 		k = len(s3)
