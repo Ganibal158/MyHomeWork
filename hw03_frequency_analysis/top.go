8856f9706f7db89ff3a00b01b83bcfcc93bdc083
Kirill <kuznetsokirill@yandex.ru>
2025-03-09 20:27:31 +0300

    HW3 is changed2

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,20 +1,16 @@
 package hw03frequencyanalysis
 
 import (
+	"fmt"
 	"sort"
 	"strings"
 )
 
 func Top10(str1 string) []string {
 	var top1 []string
-	s := strings.Fields(str1)  // слайс из исходной строки
-	var s2 []int               // слайс для подсчёта частоты слова
-	var s3 []string            // слайс уникальных значений
-	type WordAndIndex struct { // структура для сортировки с парами "слово"-"частота встреч"
-		word  string
-		count int
-	}
-	var worsAndIndexes []WordAndIndex = make([]WordAndIndex, 0, 100)
+	s := strings.Fields(str1) // слайс из исходной строки
+	var s2 []int              // слайс для подсчёта частоты слова
+	var s3 []string           // слайс уникальных значений
 	for i, val1 := range s {
 		index := 0
 		check := true
@@ -42,23 +38,25 @@ func Top10(str1 string) []string {
 			s2 = append(s2, index)
 		}
 	}
-	for id := range s2 { // сортируем попарно
-		worsAndIndexes = append(worsAndIndexes, WordAndIndex{word: s3[id], count: s2[id]})
+	WordAndIndex := make(map[string]int)
+	for id := range s2 {
+		WordAndIndex[s3[id]] = s2[id]
 	}
-	sort.Slice(worsAndIndexes, func(i, j int) bool {
-		if worsAndIndexes[i].count == worsAndIndexes[j].count {
-			return worsAndIndexes[i].word < worsAndIndexes[j].word
+	sort.Slice(s3, func(i, j int) bool {
+		if WordAndIndex[s3[i]] != WordAndIndex[s3[j]] {
+			return WordAndIndex[s3[i]] > WordAndIndex[s3[j]]
 		}
-		return worsAndIndexes[i].count > worsAndIndexes[j].count
+		return s3[i] < s3[j]
 	})
+	fmt.Println(s3)
 	var k int
-	if len(worsAndIndexes) < 10 {
-		k = len(worsAndIndexes)
+	if len(s3) < 10 {
+		k = len(s3)
 	} else {
 		k = 10
 	}
 	for i := 0; i < k; i++ { // формируем выходной слайс длиной k
-		top1 = append(top1, worsAndIndexes[i].word)
+		top1 = append(top1, s3[i])
 	}
 	return top1
 }
