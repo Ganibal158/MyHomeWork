93e6b3078e760f0faaf3683ff916b33d9532f009
Kirill <kuznetsokirill@yandex.ru>
2025-05-04 18:45:14 +0300

    HW5 is chenged

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -23,40 +23,44 @@ func Run(tasks []Task, n, m int) error {
 	)
 	wg.Add(n)
 	for i := 0; i < n; i++ { // Создаём n воркеров
-		go func() {
-			defer wg.Done()
-			for {
-				select {
-				case <-done: // Заврершение работы воркера при чтении/закрытии канала done
-					return
-				case task, ok := <-tasksCh: // Чтение задачи из канала
-					if !ok {
-						return
-					}
-					if err := task(); err != nil {
-						if int(atomic.AddInt32(&errCount, 1)) >= m {
-							once.Do(func() { // безопасное закрытие канала
-								close(done)
-							})
-						}
-					}
-				}
-			}
-		}()
+		go startWorker(&wg, tasksCh, done, &errCount, m, &once)
 	}
-	go func() { // Горутина для передачи задач через канал
-		defer close(tasksCh)
-		for _, task := range tasks {
-			select {
-			case <-done:
-				return
-			case tasksCh <- task:
-			}
-		}
-	}()
+	go enqueueTasks(tasks, tasksCh, done)
 	wg.Wait()
 	if int(atomic.LoadInt32(&errCount)) >= m { // При превышении кол-ва ошибок, передаём ошибку
 		return ErrErrorsLimitExceeded
 	}
 	return nil
+}
+
+func startWorker(wg *sync.WaitGroup, tasksCh <-chan Task, done chan struct{}, errCount *int32, m int, once *sync.Once) {
+	defer wg.Done()
+	for {
+		select {
+		case <-done: // Заврершение работы воркера при чтении/закрытии канала done
+			return
+		case task, ok := <-tasksCh: // Чтение задачи из канала
+			if !ok {
+				return
+			}
+			if err := task(); err != nil {
+				if int(atomic.AddInt32(errCount, 1)) >= m {
+					once.Do(func() { // безопасное закрытие канала
+						close(done)
+					})
+				}
+			}
+		}
+	}
+}
+
+func enqueueTasks(tasks []Task, tasksCh chan<- Task, done <-chan struct{}) {
+	defer close(tasksCh)
+	for _, task := range tasks {
+		select {
+		case <-done:
+			return
+		case tasksCh <- task:
+		}
+	}
 }
